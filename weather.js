96b5969c804f271c8b3409ed578c841a01822a03
Gyugyu94 <rbalsdl1501@gmail.com>
2019-03-12 05:02:18 +0900

    web app complete!!

---
+++ weather.js
@@ -0,0 +1,61 @@
+const weather = document.querySelector(".js-weather");
+
+const API_KEY = "511f2a3ecb474b7a60bbcc4b1571f94e";
+const COORDS = "coords";
+
+function getWeather(lat, lon){
+    fetch(
+        `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+        )
+        .then(function(response) {
+            return  response.json();
+        }).then(function(json){
+            const temp = json.main.temp;
+            const city = json.name;
+            weather.innerText = `temp: ${temp}℃/ loc: ${city}`;
+        });
+}
+
+
+
+function saveCoords(coordsObj){
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+
+function handleGeoSucces(position){
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = {
+        latitude,
+        longitude
+    };
+    saveCoords(coordsObj);
+    getWeather(latitude,longitude);
+}
+
+function handleGeoError(){
+    console.log("Cant access geo loaction");
+}
+
+function askForCoords(){
+    navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
+}
+
+function loadCoords(){
+    const loadedCoords = localStorage.getItem(COORDS);
+    if(loadedCoords === null)
+    {
+        askForCoords();
+    }
+    else{
+        const parsedCoords = JSON.parse(loadedCoords);
+        getWeather(parsedCoords.latitude, parsedCoords.longitude);
+    }
+}
+
+function init(){
+    loadCoords();
+}
+
+init();
